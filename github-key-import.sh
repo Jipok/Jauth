0c1d31ca06a27a0784b72850236861c4b2bc0c70
Jipok <github@jipok.ru>
2024-02-12 01:19:55 +0500

    github-key-import.sh: support multiple keys

--- github-key-import.sh
+++ github-key-import.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-file="${1:-$HOME/.ssh/authorized_keys}" 
+file="${1:-$HOME/authorized_keys}"
 
 search() {
-	q=$( echo $@ | tr " " '+' )
-	curl -s "https://github.com/search?q=$q&ref=opensearch&type=users" |\
-        	jq -r '.payload.results.[] | .login + " {" + .name + "}"' |\
-		sed 's/{}//g'
+        q=$( echo $@ | tr " " '+' )
+        curl -s "https://github.com/search?q=$q&ref=opensearch&type=users" |\
+                jq -r '.payload.results.[] | .login + " {" + .name + "}"' |\
+                sed 's/{}//g'
 }
 export -f search
 
 username=$(echo "" | fzf --bind "change:reload-sync:search {q}" | cut -f1 -d' ')
-key=$(curl -s "https://github.com/$username.keys" | head -n1)
+keys=$(curl -s "https://github.com/$username.keys" | sed "s/\$/ $username/")
 
-echo "Public key: $key"
+echo "Public keys: $keys"
 echo "Username: $username"
-[ -z "$@" ] && echo "Filename not provided via cmdline. Using default one:"
+[ -z "$@" ] && echo -e "\e[0;33mFilename not provided via cmdline. Using default one:\e[m"
 echo "Append to: $file"
 
 read -r -p "Are you sure? [Y/n]" response
 response=${response,,} # tolower
 if [[ $response =~ ^(y| ) ]] || [[ -z $response ]]; then
-	echo "$key $username" >> "$file"
+        echo "$keys" >> "$file"
+        echo "DONE"
+else
+        echo "CANCELED"
 fi
